b0d5b84828793519d57a4437905da367cc0445e6
EthanJF <ethanjosephfournier@gmail.com>
2020-06-09 16:03:54 -0400

    added login functionality

--- wow-alt-tracker-railsapi/app/controllers/users_controller.rb
+++ wow-alt-tracker-railsapi/app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
+  
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user
+        else
+            render json: {
+                errors: user.errors.full_messages
+            }
+        end
+    end
+
+    def update
+    user = User.find(params[:id])
+    user.update(user_params)
+       if user.valid?
+        render json: user
+    else
+        render json: {
+            errors: user.errors.full_messages
+        }
+    end
+  end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: user
+    end
+
+
+  private
+
+  def user_params
+      params.require(:user).permit(:username, :password)
+  end
 end
--- wow-alt-tracker-railsapi/app/models/user.rb
+++ wow-alt-tracker-railsapi/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :characters
+
+    validates_presence_of :username, :password
+    validates_uniqueness_of :username, :case_sensitive => false
+    
 end
